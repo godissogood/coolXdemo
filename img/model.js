4a2a87a7ca7faf81881b55e19394b20a7dc04c58
godissogood <xiayewei@163.com>
2019-07-03 22:20:14 +0800

    JS右侧功能待完善

---
+++ img/model.js
@@ -0,0 +1,7 @@
+var closeR = document.getElementById('gb');
+var rig = document.getElementById('rc');
+
+function closer() {
+    rig.style.right = "-40px";
+}
+closeR.addEventListener("click", closer);
