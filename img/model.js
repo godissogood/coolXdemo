575d3e0d65009f5762891e66b3d466b29e71cb3e
godissogood <xiayewei@163.com>
2019-07-04 16:52:55 +0800

    首页右侧展开显示的JS动画

--- img/model.js
+++ img/model.js
@@ -1,7 +1,13 @@
 var closeR = document.getElementById('gb');
 var rig = document.getElementById('rc');
-
-function closer() {
-    rig.style.right = "-40px";
-}
+var dk = document.getElementById('dk');
+function closer () {
+  rig.style.right = "-40px";
+  dk.style.right = "0px";
+}    
+function open(){
+  dk.style.right = "-40px";
+rig.style.right = "0px";
+}  
 closeR.addEventListener("click", closer);
+dk.addEventListener('click',open);
